0e0e76a44e96a8d92673cfe9625c328b56497a66
Hedi <hedi@amdox>
2021-11-09 20:36:04 +0800

    Add example.

--- go.mod
+++ go.mod
@@ -6,16 +6,16 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/notedit/janus-go v0.0.0-20210115013133-fdce1b146d0e // indirect
-	github.com/pion/datachannel v1.4.21 // indirect
-	github.com/pion/dtls/v2 v2.0.9 // indirect
+	github.com/pion/datachannel v1.5.0 // indirect
+	github.com/pion/dtls/v2 v2.0.10 // indirect
 	github.com/pion/example-webrtc-applications/v3 v3.0.4 // indirect
-	github.com/pion/ice/v2 v2.1.12 // indirect
-	github.com/pion/interceptor v0.0.15 // indirect
+	github.com/pion/ice/v2 v2.1.13 // indirect
+	github.com/pion/interceptor v0.1.0 // indirect
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/mdns v0.0.5 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtcp v1.2.6 // indirect
-	github.com/pion/rtp v1.7.1 // indirect
+	github.com/pion/rtcp v1.2.8 // indirect
+	github.com/pion/rtp v1.7.4 // indirect
 	github.com/pion/sctp v1.7.12 // indirect
 	github.com/pion/sdp/v3 v3.0.4 // indirect
 	github.com/pion/srtp/v2 v2.0.5 // indirect
@@ -23,11 +23,11 @@ require (
 	github.com/pion/transport v0.12.3 // indirect
 	github.com/pion/turn/v2 v2.0.5 // indirect
 	github.com/pion/udp v0.1.1 // indirect
-	github.com/pion/webrtc/v3 v3.1.0-beta.3 // indirect
+	github.com/pion/webrtc/v3 v3.1.7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/xid v1.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20210812204632-0ba0e8f03122 // indirect
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import (
 	gst "RTSPSender/internal/gstreamer-src"
 	janus "RTSPSender/internal/janus"
 	"fmt"
+	"github.com/pion/interceptor"
 	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/h264reader"
+	"github.com/pion/webrtc/v3/pkg/media/ivfreader"
+	"io"
 	"log"
+	"os"
 	"time"
 )
 
@@ -42,7 +48,7 @@ func main() {
 	}
 
 	// Create a new RTCPeerConnection
-	peerConnection, err := webrtc.NewPeerConnection(config)
+	peerConnection, err := NewPeerConnection(config)
 	if err != nil {
 		panic(err)
 	}
@@ -52,20 +58,27 @@ func main() {
 	})
 
 	// Create an audio track
-	opusTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "audio/opus"}, "audio", "pion")
-	if err != nil {
-		panic(err)
-	} else if _, err = peerConnection.AddTrack(opusTrack); err != nil {
-		panic(err)
-	}
+	//opusTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus}, "audio", "pion")
+	//if err != nil {
+	//	panic(err)
+	//} else if _, err = peerConnection.AddTrack(opusTrack); err != nil {
+	//	panic(err)
+	//}
 
 	// Create a video track
-	vp8Track, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/h264"}, "video", "pion")
+	videoRTCPFeedback := []webrtc.RTCPFeedback{{"goog-remb", ""}, {"ccm", "fir"}, {"nack", ""}, {"nack", "pli"}}
+	h264Codec := webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264, ClockRate: 90000, SDPFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f", RTCPFeedback: videoRTCPFeedback}
+	vp8Track, err := webrtc.NewTrackLocalStaticSample(h264Codec, "video", "pion")
 	if err != nil {
 		panic(err)
 	} else if _, err = peerConnection.AddTrack(vp8Track); err != nil {
 		panic(err)
 	}
+
+	//const vp8Filename = "/Users/amdox/Downloads/vp8.ivf"
+	//loadVP8VideoTrackFromFile(peerConnection, vp8Filename)
+	//const h264Filename = "/Users/amdox/Downloads/cam2.h264"
+	//loadH264VideoTrackFromFile(peerConnection, h264Filename)
 
 	offer, err := peerConnection.CreateOffer(nil)
 	if err != nil {
@@ -84,7 +97,7 @@ func main() {
 	// in a production application you should exchange ICE Candidates via OnICECandidate
 	<-gatherComplete
 
-	gateway, err := janus.Connect("ws://localhost:8188/janus")
+	gateway, err := janus.Connect("ws://192.168.5.105:8188")
 	if err != nil {
 		panic(err)
 	}
@@ -114,8 +127,9 @@ func main() {
 	_, err = handle.Message(map[string]interface{}{
 		"request": "join",
 		"ptype":   "publisher",
-		"room":    1234,
+		"room":    123456,
 		"id":      1,
+		"display":	"Golang Client",
 	}, nil)
 	if err != nil {
 		panic(err)
@@ -123,7 +137,7 @@ func main() {
 
 	msg, err := handle.Message(map[string]interface{}{
 		"request": "publish",
-		"audio":   true,
+		"audio":   false,
 		"video":   true,
 		"data":    false,
 	}, map[string]interface{}{
@@ -145,9 +159,158 @@ func main() {
 		}
 
 		// Start pushing buffers on these tracks
-		gst.CreatePipeline("opus", []*webrtc.TrackLocalStaticSample{opusTrack}, "audiotestsrc").Start()
-		gst.CreatePipeline("vp8", []*webrtc.TrackLocalStaticSample{vp8Track}, "videotestsrc").Start()
+		videoPipeline := "filesrc location=/Users/amdox/Downloads/cam1.mp4 ! qtdemux ! queue ! h264parse ! video/x-h264,alignment=nal,stream-format=byte-stream ! appsink emit-signals=True name=h264_sink"
+		//gst.CreatePipeline("opus", []*webrtc.TrackLocalStaticSample{opusTrack}, "audiotestsrc").Start()
+		gst.CreatePipeline("h264", []*webrtc.TrackLocalStaticSample{vp8Track}, videoPipeline).Start()
 	}
 
 	select {}
+}
+
+func NewPeerConnection(configuration webrtc.Configuration) (*webrtc.PeerConnection, error) {
+	m := &webrtc.MediaEngine{}
+
+	videoRTCPFeedback := []webrtc.RTCPFeedback{{"goog-remb", ""}, {"ccm", "fir"}, {"nack", ""}, {"nack", "pli"}}
+	codec:= webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264, ClockRate: 90000, SDPFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f", RTCPFeedback: videoRTCPFeedback}
+
+	codecParam := webrtc.RTPCodecParameters{
+		RTPCodecCapability: codec,
+		PayloadType:        102,
+	}
+
+	if err := m.RegisterCodec(codecParam, webrtc.RTPCodecTypeVideo); err != nil {
+		return nil, err
+	}
+
+	i := &interceptor.Registry{}
+	if err := webrtc.RegisterDefaultInterceptors(m, i); err != nil {
+		return nil, err
+	}
+
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(m), webrtc.WithInterceptorRegistry(i))
+	return api.NewPeerConnection(configuration)
+}
+
+func loadH264VideoTrackFromFile(pc *webrtc.PeerConnection, filename string) {
+	_, err := os.Stat(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	fileExist := !os.IsNotExist(err)
+	if fileExist {
+
+		videoRTCPFeedback := []webrtc.RTCPFeedback{{"goog-remb", ""}, {"ccm", "fir"}, {"nack", ""}, {"nack", "pli"}}
+		h264Codec := webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264, ClockRate: 90000, SDPFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f", RTCPFeedback: videoRTCPFeedback}
+
+		videoTrack, videoTrackErr := webrtc.NewTrackLocalStaticSample(h264Codec, "video", "pion")
+		if videoTrackErr != nil {
+			panic(videoTrackErr)
+		}
+		rtpSender, videoTrackErr := pc.AddTrack(videoTrack)
+		if videoTrackErr != nil {
+			panic(videoTrackErr)
+		}
+
+		go func() {
+			rtcpBuf := make([]byte, 1500)
+			for {
+				if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+					return
+				}
+			}
+		}()
+
+		const h264FrameDuration = time.Millisecond * 33
+
+		go func() {
+			file, h264Err := os.Open(filename)
+			if h264Err != nil {
+				panic(h264Err)
+			}
+
+			reader, h264Err := h264reader.NewReader(file)
+			if h264Err != nil {
+				panic(h264Err)
+			}
+
+			ticker := time.NewTicker(h264FrameDuration)
+			for ; true; <-ticker.C {
+				nal, h264Err := reader.NextNAL()
+
+				if h264Err == io.EOF {
+					fmt.Printf("Read file fininshed! exit program.")
+					os.Exit(0)
+				}
+
+				if h264Err != nil {
+					panic(h264Err)
+				}
+
+				if h264Err = videoTrack.WriteSample(media.Sample{Data: nal.Data, Duration: time.Second}); h264Err != nil {
+					panic(h264Err)
+				}
+			}
+		}()
+	}
+}
+
+func loadVP8VideoTrackFromFile(pc *webrtc.PeerConnection, filename string) {
+	_, err := os.Stat(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	fileExist := !os.IsNotExist(err)
+	if fileExist {
+		// Create a video track
+		videoTrack, videoTrackErr := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeVP8}, "video", "pion")
+		if videoTrackErr != nil {
+			panic(videoTrackErr)
+		}
+
+		rtpSender, videoTrackErr := pc.AddTrack(videoTrack)
+		if videoTrackErr != nil {
+			panic(videoTrackErr)
+		}
+
+		go func() {
+			rtcpBuf := make([]byte, 1500)
+			for {
+				if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+					return
+				}
+			}
+		}()
+
+		go func() {
+			// Open a IVF file and start reading using our IVFReader
+			file, ivfErr := os.Open(filename)
+			if ivfErr != nil {
+				panic(ivfErr)
+			}
+
+			ivf, header, ivfErr := ivfreader.NewWith(file)
+			if ivfErr != nil {
+				panic(ivfErr)
+			}
+
+			ticker := time.NewTicker(time.Millisecond * time.Duration((float32(header.TimebaseNumerator)/float32(header.TimebaseDenominator))*1000))
+			for ; true; <-ticker.C {
+				frame, _, ivfErr := ivf.ParseNextFrame()
+				if ivfErr == io.EOF {
+					fmt.Printf("All video frames parsed and sent")
+					os.Exit(0)
+				}
+
+				if ivfErr != nil {
+					panic(ivfErr)
+				}
+
+				if ivfErr = videoTrack.WriteSample(media.Sample{Data: frame, Duration: time.Second}); ivfErr != nil {
+					panic(ivfErr)
+				}
+			}
+		}()
+	}
 }
